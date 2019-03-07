31401630fd1afb451e7099309d873615c273070c
Ojtinjacar <Ojtinjacar@unal.edu.co>
2019-03-06 20:00:33 -0500

    qqqq
    aaaaaa

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,5 +2,20 @@ const express = require('express');
 const router= express.Router();
 
 //pool hace referencia a la conexion a la bd
-const pool = require('../database')
+const pool = require('../database');
+
+router.get('/Login',(req,res)=>{
+    res.render('links/Login');
+});
+    
+router.post('/Login', async (req,res)=>{
+    const {User, Password} = req.body;
+    const LoginUser = {
+        User,
+        Password
+    };
+    await pool.query('INSERT INTO Usuario set ?',[LoginUser]);
+    res.send('reiceved');   
+});
+
 module.exports = router;
