0aa46d0ae5c6eaeabb3e50dd99b3c9094c13676b
Ojtinjacar <Ojtinjacar@unal.edu.co>
2019-03-09 04:38:40 -0500

    Avance 3

--- src/routes/links.js
+++ src/routes/links.js
@@ -12,6 +12,10 @@ router.get('/CreateUser',(req,res)=>{
     res.render('links/CreateUser');
 });
 
+router.get('/ListEmployees',async (req,res) => {
+    const empleados =  await pool.query('SELECT * FROM empleado');
+    res.render("links/ListEmployees",{empleados: empleados}); 
+});
 
 
 router.post('/Login', async (req,res)=>{
@@ -23,9 +27,13 @@ router.post('/Login', async (req,res)=>{
     await pool.query('INSERT INTO EMPLEADO set ?',[LoginUser]);
     res.send('reiceved');   
 });
+router.post('/ListEmployees', async (req,res)=>{
+    const Hola = req.params.id;
+
+});
+
 
 router.post('/CreateUser', async (req,res)=>{
-    console.log(req.body);
     const {   
                 Identificacion,
                 Contrasena, 
@@ -59,12 +67,48 @@ router.post('/CreateUser', async (req,res)=>{
 
     };
     await pool.query('INSERT INTO empleado set ?',[RegisterUser]);
-    res.send('reiceved');   
+    res.redirect('CreateUser');
+  
 });
 
-router.get('/ListEmployees',async (req,res) => {
-    const empleados =  await pool.query('SELECT * FROM empleado');
-    console.log(empleados); 
-    res.render("links/ListEmployees",{empleados: empleados}); 
+
+router.get('/Delete/:IDENTIFICACION', async(req,res)=>{
+    const {IDENTIFICACION} = req.params;
+    pool.query('DELETE FROM empleado WHERE IDENTIFICACION = ?',[IDENTIFICACION]);
+    res.redirect('/links/ListEmployees');
+
 });
+
+router.get('/Edit/:IDENTIFICACION', async(req,res)=>{
+    const {IDENTIFICACION} = req.params;
+    const data = await pool.query('SELECT * FROM empleado WHERE IDENTIFICACION = ?',[IDENTIFICACION]);
+    console.log(data);
+    res.render('links/EditUser',{data:data[0]});
+});
+
+router.post('/edit/:id', async (req,res)=>{
+    const {IDENTIFICACION} = req.params;
+    const {   
+                Correo,
+                SueldoBasico,
+                Pensiones,
+                Cesantias,
+                EPS,
+                Cargo,
+            } = req.body;
+    const RegisterUser = 
+    {
+        Correo,
+        SueldoBasico,
+        Pensiones,
+        EPS,
+        Cargo,
+        Cesantias
+
+    };
+    pool.query('UPDATE empleado set ? WHERE IDENTIFICACION = ?',[RegisterUser,IDENTIFICACION]);
+    res.redirect('/links/ListEmployees');
+  
+});
+
 module.exports = router;
